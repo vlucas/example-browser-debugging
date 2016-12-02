15dd52a3f48ed8b6c820f34a9bc9c50eecb5a3c2
Vance Lucas <vance@vancelucas.com>
2016-12-01 21:07:26 -0600

    Initital conversion to vanilla JS (abandoned)

--- src/client/index.js
+++ src/client/index.js
@@ -1,13 +1,50 @@
 'use strict';
 
-import App from '../components/App';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import Home from '../components/Home';
+// import Events from '../components/Events';
+// import Inbox from '../components/Inbox';
+// import Timer from '../components/Timer';
 
-const containerElement = document.getElementById('content');
-
-function renderComponent(component) {
-  ReactDOM.render(component, containerElement);
+function parseUrl( url ) {
+  let a = document.createElement('a');
+  a.href = url;
+  return a;
 }
 
-renderComponent(<App />);
+function getComponent(component) {
+  let options = {
+    'home': () => Home,
+    'events': () => Events,
+    'inbox': () => Inbox,
+    'timer': () => Timer,
+  };
+
+  if (typeof options[component] === 'undefined') {
+    throw new Error('Specified component not available');
+  }
+
+  return options[component]();
+}
+
+function renderComponent(componentName) {
+  let component = getComponent(componentName);
+
+  console.log('> Component: ', componentName, component);
+
+  $('#content').html(component.render());
+}
+
+function handleMenuClick(e) {
+  let url = e.target.href;
+  let componentName = parseUrl(url).hash.replace('#', '');
+
+  renderComponent(componentName);
+}
+
+
+function init() {
+  $('.side-nav a').click(handleMenuClick);
+  renderComponent('home');
+}
+
+init();
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,47 +1,35 @@
-import React from 'react';
-
 const MAGIC_NUMBER = 2;
 
-export default class Home extends React.Component {
-  constructor(props) {
-    super(props);
+function triggerError() {
+  let foo = 'bar';
 
-    this.state = {
-      MAGIC_NUMBER: '...'
-    };
+  throw new Error('This is an error that I just triggered! ' + foo);
+}
+
+function getMagicNumber() {
+  try {
+    return 2;
   }
-
-  triggerError() {
-    let foo = 'bar';
-
-    throw new Error('This is an error that I just triggered! ' + foo);
-  }
-
-  getMagicNumber() {
-    try {
-      return 2;
-    }
-    finally {
-      return 3;
-    }
-  }
-
-  showMagicNumber() {
-    this.setState({
-      MAGIC_NUMBER: this.getMagicNumber()
-    });
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>Homepage</h2>
-        <p><a className="button button-warn" onClick={() => this.triggerError()}>Trigger Error</a></p>
-        <p>&nbsp;</p>
-        <p><a className="button" onClick={() => this.showMagicNumber()}>Show Magic Number ({MAGIC_NUMBER})</a> ({this.state.MAGIC_NUMBER})</p>
-        <p>&nbsp;</p>
-        <p>Something here</p>
-      </div>
-    );
+  finally {
+    return 3;
   }
 }
+
+function showMagicNumber() {
+  $('.magic_number').text(getMagicNumber());
+}
+
+function render() {
+  return `
+    <div>
+      <h2>Homepage</h2>
+      <p><a class="button button-warn" onClick=${() => triggerError()}>Trigger Error</a></p>
+      <p>&nbsp;</p>
+      <p><a class="button" onClick="">Show Magic Number ({MAGIC_NUMBER})</a> ({this.state.MAGIC_NUMBER})</p>
+      <p>&nbsp;</p>
+      <p>Something here</p>
+    </div>
+  `;
+}
+
+export default { render };
